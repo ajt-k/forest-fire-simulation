2196fdfa146cfd60d1ea05970f2c48140ab37d41
Alex Thomas <106121124+ajt-k@users.noreply.github.com>
2022-05-28 11:17:35 -0400

    Updated Spectrum Functions.py
    Colormap fixed, spread fixed. Very cool.

--- Spectrum-Functions.py
+++ Spectrum-Functions.py
@@ -4,7 +4,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import colors, animation
+from matplotlib import  animation
+import matplotlib.colors as mcolors
 from perlin_numpy import (
     generate_perlin_noise_2d, generate_fractal_noise_2d
 )
@@ -13,34 +14,37 @@ fire = 2
 
 
 def fire_spread(size,forest,fire_tracker):
-    region = [(0,1),(0,-1),(-1,0),(1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
-    prob_spread = 0.2
-    fire = 2
+    region = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+    prob_spread = 0.3
     lst = []
     for x in range(1,size-1):
         for y in range(1,size-1):
-                for p,q in region:
-                    if fire_tracker[x,y] > 1 and fire_tracker[x+p,y+q] < 1 and np.random.random() <= prob_spread*forest[x+p,y+q]:
-                        fire_tracker[x+p,y+q] = 1+forest[x+p,y+q]
-                        lst.append((p,q))
-                            
-                           
+            if fire_tracker[x,y] > 1:
+                lst.append((x,y))
                     
-    for m in region:
-        print(m, lst.count(m))
+                    
+    for x,y in lst:
+        for p,q in region:
+            if fire_tracker[x+p,y+q] < 1 and np.random.random() <= prob_spread*forest[x+p,y+q]:
+                        fire_tracker[x+p,y+q] = 1+forest[x+p,y+q]
+                        
     return forest, fire_tracker
+
+
+
 
 
 def initialize_forest(seed,size):
     np.random.seed(seed)
- # maybe add rocks in 
+   # rock = 0
     forest = generate_fractal_noise_2d((size, size), (8, 8), 5)
 
     for x in range(0,size):
         for y in range(0,size):
             forest[x,y] +=1
             forest[x,y] = forest[x,y]*1/2
-           
+            #if np.random.random()<0.05:
+               # forest[x,y] = rock
     
     
     fire_map = np.zeros((size,size),dtype=float)
@@ -49,20 +53,18 @@ def initialize_forest(seed,size):
     return forest, fire_map
 
 def burn_out(size,forest,fire_tracker):
-    fire = 2
-    lst = []
+
     for x in range(1,size-1):
         for y in range(1,size-1):
             
             fire_tracker[x,y] = fire_tracker[x,y] - (1/6)*forest[x,y]
-            if fire_tracker[x,y] > 1:
-                forest[x,y] = forest[x,y]- 0.3
+            
     
     return forest,fire_tracker
 
 
 def combine_grids(size,forest,fire_tracker):
-    fire = 2
+ 
     show_grid = np.zeros((size,size),dtype=float)
     
     for x in range(1,size-1):
@@ -91,24 +93,23 @@ def animate(i):
     
     return MATRIX,FOREST,FIRE_MAP
 
+colors1 = plt.cm.Greens(np.linspace(0., 1, 128))
+colors2 = plt.cm.Reds(np.linspace(0, 1, 128))
+
+# combine them and build a new colormap
+colors = np.vstack((colors1, colors2))
+cmap = mcolors.LinearSegmentedColormap.from_list('my_colormap', colors)
 
 fig, ax = plt.subplots()
 SIZE = 256
 SEED = np.random.randint(0,1000)
 FOREST, FIRE_MAP = initialize_forest(SEED,SIZE)
-MATRIX = ax.matshow(FOREST, cmap='RdYlGn_r')
+MATRIX = ax.matshow(FOREST,cmap = cmap,vmin = 0,vmax= 2)
 
-FIRE_MAP[128,128] = fire   
+
+FIRE_MAP[128,128] = 2  
 np.random.seed(SEED)
 ani = animation.FuncAnimation(fig, animate, frames=20000, interval=500)
 
 plt.show()
 
-FOREST, FIRE_MAP = initialize_forest(SEED,SIZE)
-MATRIX = ax.matshow(FOREST, cmap='RdYlGn_r')
-
-FIRE_MAP[128,128] = fire   
-np.random.seed(SEED)
-ani = animation.FuncAnimation(fig, animate, frames=20000, interval=500)
-
-plt.show()
